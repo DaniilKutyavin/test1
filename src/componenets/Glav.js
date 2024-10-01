c804d13f0451579c50a908ac913ef01db65bb07e
DaniilKutyavin <daniilkkutyavin@yandex.ru>
2024-10-01 17:32:20 +0500

    правки 4

--- src/componenets/Glav.js
+++ src/componenets/Glav.js
@@ -11,10 +11,10 @@ import videoSrc from "../img/main.gif";
 import PopupInfo from './PopupInfo';
 
 import img1 from "../img/kb1.png";
-import img2 from "../img/kb2.png";
+import img2 from "../img/img9.jpg";
 import img3 from "../img/kb3.png";
-import img4 from "../img/kb4.png";
-import img5 from "../img/kb5.png";
+import img4 from "../img/img10.jpg";
+import img5 from "../img/img11.jpg";
 import img6 from "../img/img6.jpg";
 import img7 from "../img/img7.jpg";
 import img8 from "../img/img8.jpg";
